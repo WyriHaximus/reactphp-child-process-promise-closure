20dbe40aac953cd807bf65f60ad639aecffa2ada
Cees-Jan Kiewiet <ceesjank@gmail.com>
2022-02-19 22:31:30 +0100

    Next Gen QA (PHP 7.4)

--- src/functions_include.php
+++ src/functions_include.php
@@ -1,7 +1,11 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace WyriHaximus\React;
 
-if (!\function_exists('WyriHaximus\React\childProcessPromiseClosure')) {
+use function function_exists;
+
+if (! function_exists('WyriHaximus\React\childProcessPromiseClosure')) {
     require __DIR__ . '/functions.php';
 }
